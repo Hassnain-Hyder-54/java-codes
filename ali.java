53c42e80ef5d9e277ff20789b75fb786e01c513d
Hassnain Hyder <alishan45202164@gmail.com>
2025-10-17 10:39:40 +0500

    Added another line

--- ali.java
+++ ali.java
@@ -1,5 +1,9 @@
 public class ali {
     public static void main(String[] args) {
         System.out.println("hello" );
-    }
+                System.out.println("world");
+
+        
+    
+        }
 }
